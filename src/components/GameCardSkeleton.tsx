f447e08400228cf6f0a75a70be48ab6e7db07916
Groux0705 <tthemszf@gmail.com>
2024-01-08 07:43:18 +0800

    展示骨架屏

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <>
+      <Card height="300px" borderRadius={10} overflow="hidden">
+        <Skeleton></Skeleton>
+        <CardBody>
+          <SkeletonText></SkeletonText>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default GameCardSkeleton;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,8 +1,10 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 const GameGrid = () => {
-  const { games, error } = useGame();
+  const { games, error, isLoading } = useGame();
+  const skeletons = [0, 1, 2, 3, 4, 5];
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -11,6 +13,8 @@ const GameGrid = () => {
         spacing={10}
         padding="10px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton></GameCardSkeleton>)}
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
