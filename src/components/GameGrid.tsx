da8603bf503b0745eef023c620184329a9b2f79f
Groux0705 <tthemszf@gmail.com>
2024-01-07 17:00:35 +0800

    创建了一个useGame的hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResult {
-  count: number;
-  results: Game[];
-}
-
+import { Text } from "@chakra-ui/react";
+import useGame from "../hooks/useGame";
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResult>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((error) => setError(error.message));
-  });
-
+  const { games, error } = useGame();
   return (
     <>
-      <p>{error}</p>
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
