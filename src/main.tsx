eada1e82b1a643046c1b37bfb276bb741fda41d9
Groux0705 <tthemszf@gmail.com>
2024-01-24 20:44:39 +0800

    设置路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
 
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import App from "./App";
 import "./index.css";
 
 import theme from "./theme";
@@ -16,10 +17,10 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
         <ColorModeScript
           initialColorMode={theme.config.initialColorMode}
         ></ColorModeScript>
-        <App />
         <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
